a40c4322a6c612811ed5c5a888e51b6390ffdad1
abhayrsr <abhayrajsinghrana123@gmail.com>
2025-07-06 16:04:35 +0530

    feat: added hero.astro

--- dev/src/components/ui/NavBar.tsx
+++ dev/src/components/ui/NavBar.tsx
@@ -64,7 +64,7 @@ export function NavBar({
   return (
     <div
       className={cn(
-        "fixed bottom-0 sm:top-0 left-1/2 -translate-x-1/2 z-50 mb-6 sm:pt-6",
+        "mb-6 sm:pt-6",
         className,
       )}
     >
