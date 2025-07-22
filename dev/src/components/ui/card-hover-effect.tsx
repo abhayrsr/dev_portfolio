6c7f762a9d46aa9e3f52bd0748c74b94b0cca3ab
abhayrsr <abhayrajsinghrana123@gmail.com>
2025-07-23 02:52:58 +0530

    fix: card-hover-effect

--- dev/src/components/ui/card-hover-effect.tsx
+++ dev/src/components/ui/card-hover-effect.tsx
@@ -19,22 +19,22 @@ export const HoverEffect = ({
   return (
     <div
       className={cn(
-        "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10",
-        className
+        "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10 ",
+        className,
       )}
     >
       {items.map((item, idx) => (
         <a
           href={item?.link}
           key={item?.link}
-          className="relative group  block p-2 h-full w-full"
+          className="relative group  block p-2 h-full w-full "
           onMouseEnter={() => setHoveredIndex(idx)}
           onMouseLeave={() => setHoveredIndex(null)}
         >
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-gray-900 dark:bg-gray-900 block  rounded-3xl"
+                className="absolute inset-0 h-full w-full bg-gray-900 dark:bg-gray-900 block  rounded-3xl z-20"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
@@ -69,10 +69,10 @@ export const Card = ({
     <div
       className={cn(
         "rounded-2xl h-full w-full  overflow-hidden bg-transparent border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
-        className
+        className,
       )}
     >
-      <div className="relative z-50">
+      <div className="relative ">
         <div className="p-4">{children}</div>
       </div>
     </div>
@@ -86,7 +86,12 @@ export const CardTitle = ({
   children: React.ReactNode;
 }) => {
   return (
-    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+    <h4
+      className={cn(
+        "text-zinc-100 font-bold tracking-wide mt-4 z-100",
+        className,
+      )}
+    >
       {children}
     </h4>
   );
@@ -101,14 +106,11 @@ export const CardDescription = ({
   return (
     <p
       className={cn(
-        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
-        className
+        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm z-100",
+        className,
       )}
     >
       {children}
     </p>
   );
 };
-
-
-
